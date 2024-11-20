96f9091a4e9b05df4c323cf0f4978649dadcb406
Davi Silva <davisilvaphoto@gmail.com>
2024-11-20 20:17:23 -0300

    code-first approach in graphql

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import 'reflect-metadata'
+
+import { ApolloServer } from 'apollo-server'
+import { buildSchema } from 'type-graphql'
+import { AppointmentsResolver } from './resolvers/appointments-resolver'
+
+import path from 'node:path'
+
+async function main() {
+  const schema = await buildSchema({
+    resolvers: [AppointmentsResolver],
+    emitSchemaFile: path.resolve(__dirname, 'schema.gql'),
+  })
+
+  const server = new ApolloServer({ schema })
+
+  const { url } = await server.listen()
+
+  console.log(`🚀 Server running on ${url}`)
+}
+
+main()
