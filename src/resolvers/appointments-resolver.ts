c08e2e032ca03f572a63f03153f17efb54a863b6
Davi Silva <davisilvaphoto@gmail.com>
2024-11-20 21:01:50 -0300

    code-first approach in graphql - done

--- src/resolvers/appointments-resolver.ts
+++ src/resolvers/appointments-resolver.ts
@@ -1,9 +1,45 @@
-import { Query, Resolver } from 'type-graphql'
+import {
+  Arg,
+  FieldResolver,
+  Mutation,
+  Query,
+  Resolver,
+  Root,
+} from 'type-graphql'
+import { CreateAppointmentInput } from '../dtos/create-appointment-input'
+import { Appointment } from '../dtos/models/appointment-model'
+import { Customer } from '../dtos/models/customer-model'
 
-@Resolver()
+@Resolver(() => Appointment)
 export class AppointmentsResolver {
-  @Query(() => String)
-  async helloWorld() {
-    return 'Hello World'
+  @Query(() => [Appointment])
+  async appointments() {
+    return [
+      {
+        startsAt: new Date(),
+        endsAt: new Date(),
+      },
+    ]
+  }
+
+  @Mutation(() => Appointment)
+  async createAppointment(
+    @Arg('data', () => CreateAppointmentInput) data: CreateAppointmentInput
+  ) {
+    const appointment = {
+      startsAt: data.startsAt,
+      endsAt: data.endsAt,
+    }
+
+    return appointment
+  }
+
+  @FieldResolver(() => Customer)
+  async customer(@Root() appointment: Appointment) {
+    console.log(appointment)
+
+    return {
+      name: 'Davi Silva',
+    }
   }
 }
